7fb9b29186448d88d1f7813434680efbb6f72a57
Tobias Graf <tobiasgraf87@gmail.com>
2019-02-14 21:56:21 +0100

    fixed tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,43 +1,68 @@
-import 'jsdom-global/register';
-import React from 'react';
+import React from "react"
 
-import {EventBus} from '../eventBus'
-import Enzyme from 'enzyme';
+import { EventBus } from "../eventBus"
+import Enzyme from "enzyme"
 
-import firebase from 'firebase'
-import App from './App';
-import _ from 'lodash'
+import firebase from "firebase"
+import App from "./App"
+import _ from "lodash"
 
-import MakeTestCard from './MakeTestCard'
+import MakeTestCard from "./MakeTestCard"
 
 const TestCard = MakeTestCard()
 
-describe('Mount the app Card', () => {
+describe("Mount the app Card", () => {
   const Bus = new EventBus()
-  const appWrapper = Enzyme.shallow(<App bus={Bus}/>)
+  const appWrapper = Enzyme.shallow(<App bus={Bus} />)
 
-  test('save Function from App', async (done) => {
+  test("save Function from App", async done => {
     firebase.auth.Auth.Persistence.LOCAL = firebase.auth.Auth.Persistence.NONE
-    await appWrapper.instance().login({login: 'test@42tg.de', password:'test1234'}).catch(err => done.fail(err))
-    await appWrapper.instance().saveCard(TestCard).catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .login({ login: "test@42tg.de", password: "test1234" })
+      .catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .saveCard(TestCard)
+      .catch(err => done.fail(err))
     done()
   })
 
-  test('delete Function from App', (done) => {
-    appWrapper.instance().deleteCard(TestCard.id).then(done).catch(err => done.fail(err))
+  test("delete Function from App", done => {
+    appWrapper
+      .instance()
+      .deleteCard(TestCard.id)
+      .then(done)
+      .catch(err => done.fail(err))
   })
 
-  test('reset Function from App', (done) => {
-    appWrapper.instance().resetAll().then(done).catch(err => done.fail(err))
+  test("reset Function from App", done => {
+    appWrapper
+      .instance()
+      .resetAll()
+      .then(done)
+      .catch(err => done.fail(err))
   })
 
-  test('complete functionality from App', async (done) => {
+  test("complete functionality from App", async done => {
     const Card1 = _.clone(TestCard)
     const Card2 = _.clone(TestCard)
-    await appWrapper.instance().saveCard(Card1).catch(err => done.fail(err))
-    await appWrapper.instance().saveCard(Card2).catch(err => done.fail(err))
-    await appWrapper.instance().deleteCard(Card1.id).catch(err => done.fail(err))
-    await appWrapper.instance().resetAll().catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .saveCard(Card1)
+      .catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .saveCard(Card2)
+      .catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .deleteCard(Card1.id)
+      .catch(err => done.fail(err))
+    await appWrapper
+      .instance()
+      .resetAll()
+      .catch(err => done.fail(err))
     done()
   })
 })
--- src/components/UserBar.test.js
+++ src/components/UserBar.test.js
@@ -1,55 +1,57 @@
-import 'jsdom-global/register';
-import React from 'react';
+import React from "react"
 
-import {UserBar} from './UserBar'
-import Enzyme from 'enzyme';
+import { UserBar } from "./UserBar"
+import Enzyme from "enzyme"
 
-import {EventBus, EventTypes} from '../eventBus'
+import { EventBus, EventTypes } from "../eventBus"
 
-describe('userBar testing', () => {
+describe("userBar testing", () => {
   const bus = new EventBus()
 
-  test('mounts correctly', () => {
-    Enzyme.shallow(<UserBar bus={bus}/>)
+  test("mounts correctly", () => {
+    Enzyme.shallow(<UserBar bus={bus} />)
   })
-  test('User is logged out and it shows Loginform', () => {
-    const wrapper = Enzyme.shallow(<UserBar bus={bus}/>)
-    const result = wrapper.find('.userBar')
-    expect(result.text()).toBe('<LoginForm />')
+  test("User is logged out and it shows Loginform", () => {
+    const wrapper = Enzyme.shallow(<UserBar bus={bus} />)
+    const result = wrapper.find(".userBar")
+    expect(result.text()).toBe("<LoginForm />")
   })
 
-  test('Simulate User with Email only', () => {
+  test("Simulate User with Email only", () => {
     const user = {
-      email: 'test@42tg.de'
+      email: "test@42tg.de",
     }
-    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user}/>)
-    const findMail = wrapper.find('.userBar').find('span')
-    const findLogoutButton = wrapper.find('.userBar').find('button')
+    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user} />)
+    const findMail = wrapper.find(".userBar").find("span")
+    const findLogoutButton = wrapper.find(".userBar").find("button")
     expect(findMail.text()).toBe(user.email)
-    expect(findLogoutButton.text()).toBe('Logout')
+    expect(findLogoutButton.text()).toBe("Logout")
   })
-  test('Simulate User with DisplayName and Email only', () => {
+  test("Simulate User with DisplayName and Email only", () => {
     const user = {
-      displayName: 'Testtimonial',
-      email: 'test@42tg.de'
+      displayName: "Testtimonial",
+      email: "test@42tg.de",
     }
-    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user}/>)
-    const findMail = wrapper.find('.userBar').find('span')
-    const findLogoutButton = wrapper.find('.userBar').find('button')
+    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user} />)
+    const findMail = wrapper.find(".userBar").find("span")
+    const findLogoutButton = wrapper.find(".userBar").find("button")
     expect(findMail.text()).toBe(user.displayName)
-    expect(findLogoutButton.text()).toBe('Logout')
+    expect(findLogoutButton.text()).toBe("Logout")
   })
 
-  test('Check event handling', () => {
+  test("Check event handling", () => {
     const user = {
-      displayName: 'Testtimonial',
-      email: 'test@42tg.de'
+      displayName: "Testtimonial",
+      email: "test@42tg.de",
     }
     const callback = jest.fn()
     bus.on(EventTypes.USER_LOGOUT, callback)
-    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user}/>)
+    const wrapper = Enzyme.shallow(<UserBar bus={bus} user={user} />)
 
-    wrapper.find('.userBar').find('button').simulate('click')
+    wrapper
+      .find(".userBar")
+      .find("button")
+      .simulate("click")
     expect(callback).toBeCalled()
   })
 })
